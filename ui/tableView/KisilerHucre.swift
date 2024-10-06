b3801387cf5900b7f0505dffa4fac7bb3ea9fcd1
İzem Özer <86160580+izem2001@users.noreply.github.com>
2024-10-06 19:06:07 +0300

    Added Search Bar and Table View
    Codes have been updated with Search bar and Table View.

---
+++ ui/tableView/KisilerHucre.swift
@@ -0,0 +1,24 @@
+//
+//  KisilerHucre.swift
+//  KisilerUygulamasi
+//
+//  Created by İzem Özer on 6.10.2024.
+//
+
+import UIKit
+
+class KisilerHucre: UITableViewCell {
+    @IBOutlet weak var labelKisiAd: UILabel!
+    @IBOutlet weak var labelKisiTel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
--- ui/views/Anasayfa.swift
+++ ui/views/Anasayfa.swift
@@ -8,14 +8,27 @@
 import UIKit
 
 class Anasayfa: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var kisilerTableView: UITableView!
+    
+    var kisilerListesi = [Kisiler]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        kisilerTableView.delegate = self
+        kisilerTableView.dataSource = self
         
+        let k1 = Kisiler(kisi_id: 1, kisi_adi: "İzem", kisi_tel: "666666")
+        let k2 = Kisiler(kisi_id: 2, kisi_adi: "Bozo", kisi_tel: "222222")
+        let k3 = Kisiler(kisi_id: 3, kisi_adi: "İlhan", kisi_tel: "333333")
+        kisilerListesi.append(k1)
+        kisilerListesi.append(k2)
+        kisilerListesi.append(k3)
+
     }
-    @IBAction func butonDetay(_ sender: Any) {
-        let kisi = Kisiler(kisi_id: 1, kisi_adi: "Ahmet", kisi_tel: "1111111")
-        performSegue(withIdentifier: "toDetay", sender: kisi)
+    override func viewWillAppear(_ animated: Bool) {
+        print("Anasayfaya dönüldü")
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDetay"{
@@ -25,6 +38,47 @@ class Anasayfa: UIViewController {
             }
         }
     }
-
+}
+extension Anasayfa : UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Kişi Ara: \(searchText)")
+    }
+}
+extension Anasayfa : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kisilerListesi.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListesi[indexPath.row]
+        let hucre = tableView.dequeueReusableCell(withIdentifier: "kisilerHucre") as! KisilerHucre
+        
+        hucre.labelKisiAd.text = kisi.kisi_adi
+        hucre.labelKisiTel.text = kisi.kisi_tel
+        
+        return hucre
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let kisi = kisilerListesi[indexPath.row]
+        performSegue(withIdentifier: "toDetay", sender: kisi)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let silAction = UIContextualAction(style: .destructive, title: "Sil"){contextualAction,view,bool in
+        let kisi = self.kisilerListesi[indexPath.row]
+            
+            let alert = UIAlertController(title: "Silme İşlemi", message: "\(kisi.kisi_adi!) Silinsin mi?", preferredStyle: .alert)
+            
+            let iptalAction = UIAlertAction(title: "İptal", style: .cancel)
+            alert.addAction(iptalAction)
+            
+            let evetAction = UIAlertAction(title: "Evet", style: .destructive){ action in
+                print("Kişi Sil : \(kisi.kisi_id!)")
+            }
+            alert.addAction(evetAction)
+            self.present(alert,animated: true)
+        }
+        return UISwipeActionsConfiguration(actions: [silAction])
+    }
 }
 
